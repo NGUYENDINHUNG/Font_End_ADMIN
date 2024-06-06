e40c0df9e17f1589456cf9f63fffdf6060447db6
hung <nguyenhung21001@gmail.com>
2024-06-06 23:19:57 +0700

    Add

--- App/src/Pages/Add/Add.jsx
+++ App/src/Pages/Add/Add.jsx
@@ -1,25 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Add.css";
 import { assets } from "../../assets/assets";
 
 function Add(props) {
+  const [image, setImage] = useState(false);
+  const [data, setData] = useState({
+    name: "",
+    description: "",
+    price: "",
+    category: "Salad",
+  });
+
+  const onChangeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setData((data) => ({ ...data, [name]: value }));
+  };
+
+  useEffect(() => {
+    console.log("««««« data »»»»»", data);
+  }, [data]);
   return (
     <div className="add">
       <form className="flex-col">
         <div className="add-img-upload flex-col">
           <p>Upload Image</p>
           <label htmlFor="image">
-            <img src={assets.upload_area} alt="" />
+            <img
+              src={image ? URL.createObjectURL(image) : assets.upload_area}
+              alt=""
+            />
           </label>
-          <input type="file" id="image " hidden required />
+          <input
+            onChange={(e) => setImage(e.target.files[0])}
+            type="file"
+            id="image"
+            hidden
+            required
+          />
         </div>
         <div className="add-product-name flex-col">
           <p>Product Name</p>
-          <input type="text" name="name" placeholder="Type here" />
+          <input
+            onChange={onChangeHandler}
+            value={data.name}
+            type="text"
+            name="name"
+            placeholder="Type here"
+          />
         </div>
         <div className="add-product-description flex-col">
           <p>Product description</p>
           <textarea
+            onChange={onChangeHandler}
+            value={data.description}
             name="description"
             rows="6"
             placeholder="Write content here"
@@ -28,8 +62,8 @@ function Add(props) {
         <div className="add-category-price">
           <div className="add-cetegory flex-col">
             <p>Product category</p>
-            <select name="category">
-              <option value="">Saclad</option>
+            <select onChange={onChangeHandler} name="category">
+              <option value="">Salad</option>
               <option value="">Rolls</option>
               <option value="">Deserts</option>
               <option value="">Sandwich</option>
@@ -41,7 +75,13 @@ function Add(props) {
           </div>
           <div className="add-price flex-col">
             <p>Product price</p>
-            <input type="Number" name="price" placeholder="$20" />
+            <input
+              onChange={onChangeHandler}
+              value={data.price}
+              type="Number"
+              name="price"
+              placeholder="$20"
+            />
           </div>
         </div>
         <button type="submit" className="add-bnt">
