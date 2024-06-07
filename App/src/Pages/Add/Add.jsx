8c9e3eb664f4017b46d086d79bd36149ce68b521
hung <nguyenhung21001@gmail.com>
2024-06-07 21:58:31 +0700

    create axios

--- App/src/Pages/Add/Add.jsx
+++ App/src/Pages/Add/Add.jsx
@@ -1,8 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./Add.css";
 import { assets } from "../../assets/assets";
+import axios from "axios"
 
 function Add(props) {
+  const url = "http://localhost:4000";
   const [image, setImage] = useState(false);
   const [data, setData] = useState({
     name: "",
@@ -17,12 +19,32 @@ function Add(props) {
     setData((data) => ({ ...data, [name]: value }));
   };
 
-  useEffect(() => {
-    console.log("««««« data »»»»»", data);
-  }, [data]);
+  const  onSubmitHandler = async (event)=>{
+       event.preventDefault();
+       const formData = new FormData();
+       formData.append("name",data.name)
+       formData.append("description",data.description)
+       formData.append("price",Number(data.price))
+       formData.append("category",data.category)
+       formData.append("image",image)
+       const response = await axios.post(`${url}/api/food/add`,formData)
+       if (response.data.success) {
+          setData({
+            name: "",
+            description: "",
+            price: "",
+            category: "Salad",
+          })
+          setImage(false)
+       }else{
+
+       }
+  }
+
+  
   return (
     <div className="add">
-      <form className="flex-col">
+      <form className="flex-col" onSubmit={onSubmitHandler}>
         <div className="add-img-upload flex-col">
           <p>Upload Image</p>
           <label htmlFor="image">
